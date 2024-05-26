972c1b2a2ff395e22d56eab1b3e484056b4dbcc4
Bui Trong Vuong <bvt@cgis.io>
2024-05-26 18:38:44 +0700

    fixed build

--- src/BimModel/src/system/00-base/Utils.ts
+++ src/BimModel/src/system/00-base/Utils.ts
@@ -1,5 +1,5 @@
 import {IFC4} from "web-ifc";
-import {v4 as uuid4} from "uuid";
+import * as THREE from "three";
 export function globalId(): IFC4.IfcGloballyUniqueId {
-  return new IFC4.IfcGloballyUniqueId(uuid4());
+  return new IFC4.IfcGloballyUniqueId(THREE.MathUtils.generateUUID());
 }
