ce9edd624420d220680610528e03b2b7755f2868
Bui Trong Vuong <bvt@cgis.io>
2024-06-14 10:03:15 +0700

    fixed build

--- src/BimModel/src/system/element/Parameter/index.ts
+++ src/BimModel/src/system/element/Parameter/index.ts
@@ -1,6 +1,3 @@
 export * from "./BaseParameter";
 export * from "./BaseParameterGroup";
 export * from "./LevelParameter";
-export * from "./wall";
-export * from "./beam";
-export * from "./column";
